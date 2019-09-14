cb2ee7b597e9c4d6df90a6092f31a61da48d11bb
alarivan <alarivan@pm.me>
2019-09-14 16:46:32 -0700

    Update editor state when id changes

--- src/routes/editor.tsx
+++ src/routes/editor.tsx
@@ -1,20 +1,32 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useEffect } from 'react';
 import Container from '../components/Container';
 import { RouteComponentProps } from '@reach/router';
 import { GalleryContext } from '../GalleryContext';
-import { gridReducer, initState, initialState } from '../reducers/gridReducer';
+import {
+  gridReducer,
+  initState,
+  initialState,
+  setStateAction,
+} from '../reducers/gridReducer';
+import { TGalleryState } from '../reducers/gallery/types';
 
 interface Props extends RouteComponentProps {
   id?: string;
 }
 
+function _getGrid(id: string | undefined, state: TGalleryState) {
+  const grid = id ? state.get(id) : undefined;
+
+  return grid ? initState(grid) : initialState;
+}
+
 const Editor: React.FC<Props> = ({ id }) => {
   const { state: galleryState } = useContext(GalleryContext);
-  const grid = id ? galleryState.get(id) : null;
-  const [state, dispatch] = useReducer(
-    gridReducer,
-    grid ? initState(grid) : initialState,
-  );
+  const [state, dispatch] = useReducer(gridReducer, _getGrid(id, galleryState));
+
+  useEffect(() => {
+    dispatch(setStateAction(_getGrid(id, galleryState)));
+  }, [id]);
 
   return <Container state={state} dispatch={dispatch} />;
 };
