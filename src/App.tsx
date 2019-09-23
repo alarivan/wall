88abfa404652cc9cd7dfe8f5822537399a018234
alarivan <alarivan@pm.me>
2019-09-23 15:39:06 -0700

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Router, Link } from '@reach/router';
+import { Router } from '@reach/router';
 import Index from './routes/index';
 import Editor from './routes/editor';
 import NotFound from './routes/404';
 import { GalleryProvider } from './GalleryContext';
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import theme from './themes/default';
 
 const App: React.FC = () => {
   return (
-    <GalleryProvider>
-      <Link to='/'>Home</Link>
-      <Link to='/editor/new'>New</Link>
+    <ThemeProvider theme={theme}>
+      <GalleryProvider>
+        <Header />
 
-      <Router>
-        <Index path='/' />
-        <Editor path='/editor/:id' />
-        <Editor path='/editor/new' />
-        <NotFound default />
-      </Router>
-    </GalleryProvider>
+        <Router>
+          <Index path='/' />
+          <Editor path='/editor/:id' />
+          <Editor path='/editor/new' />
+          <NotFound default />
+        </Router>
+      </GalleryProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ src/themes/default.ts
@@ -0,0 +1,7 @@
+export default {
+  background: '#F6F8F4',
+  primary: '#6FA1F4',
+  text: '#F2A697',
+  secondary: '#4D71C2',
+  secondaryText: '4D71C2',
+};
