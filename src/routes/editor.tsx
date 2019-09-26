e9ebe4c0a45fd38d797ca6401d3173d75b8d658e
alarivan <alarivan@pm.me>
2019-09-26 14:25:47 -0700

    Add persisted reducer to editor

--- src/routes/editor.tsx
+++ src/routes/editor.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Container from '../components/Container';
 import { RouteComponentProps } from '@reach/router';
 import { GalleryContext } from '../GalleryContext';
@@ -6,6 +6,7 @@ import { TGalleryState } from '../reducers/gallery/types';
 import { initState } from '../reducers/editor/utils';
 import { initialState, gridReducer } from '../reducers/editor/reducer';
 import { setStateAction } from '../reducers/editor/actions';
+import { useGridPersistedReducer } from '../hooks/useGridPersistedReducer';
 
 interface Props extends RouteComponentProps {
   id?: string;
@@ -19,11 +20,14 @@ function _getGrid(id: string | undefined, state: TGalleryState) {
 
 const Editor: React.FC<Props> = ({ id }) => {
   const { state: galleryState } = useContext(GalleryContext);
-  const [state, dispatch] = useReducer(gridReducer, _getGrid(id, galleryState));
+  const [state, dispatch] = useGridPersistedReducer(
+    gridReducer,
+    _getGrid(id, galleryState),
+  );
 
   useEffect(() => {
     dispatch(setStateAction(_getGrid(id, galleryState)));
-  }, [id, galleryState]);
+  }, [id, galleryState, dispatch]);
 
   return <Container state={state} dispatch={dispatch} />;
 };
